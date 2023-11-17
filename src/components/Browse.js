a84ec200542e7d24b6871cd9e6c17d7f97fceb7b
dparth8421 <dparth388@gmail.com>
2023-11-17 15:33:24 +0530

    used TMDB API && added MovieSlice && custom hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import useNowPlayingMovies from "./hooks/useNowPlayingMovies";
 
 const Browse = () => {
+  // used custom hook
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
