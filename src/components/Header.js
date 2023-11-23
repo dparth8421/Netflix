f1ac3bba907ae16d03527c8c15e3c1fda4b40ddc
gagan <gagandeshpande1@gmail.com>
2023-11-23 23:59:04 +0530

    made gptsearch page multilangual

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,15 @@ import { addUser, removeUser } from "../utils/Redux/userSlice";
 import { LOGO, SUPPORTED_LANGUAGES, USER_ICON } from "./constants/constants";
 import { toggleGptSearchView } from "../utils/Redux/gptSlice";
 import lang from "../utils/languageConstant";
+import { changeLanguage } from "../utils/Redux/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -51,19 +54,38 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleGptSearchClick =() => {
+  const handleGptSearchClick = () => {
     // Toggle
-    dispatch(toggleGptSearchView())
-  }
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-40" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <select className="p-2 m-2 bg-gray-900 text-white">
-            {SUPPORTED_LANGUAGES.map(lang=><option key={lang.identifier}value={lang.identifier}>{lang.name}</option>)}
-          </select>
-          <button className="py-2 my-2 px-4 m-2 bg-purple-800 text-white rounded-lg" onClick={handleGptSearchClick}>GPT Search</button>
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 my-2 px-4 m-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch? "Home":"GPT Search"}
+          </button>
           <img className="w-12 h-12" src={user.photoURL} alt="user_icon" />
           <button className="font-bold text-white p-2" onClick={handleSignOut}>
             (Sign Out)
